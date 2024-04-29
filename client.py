9058ff7ca83734717ed8ce7d67e1ea3d9ec825b9
Bode20029 <jry.bode@gmail.com>
2024-04-30 06:56:41 +0700

    Test

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+client= socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(("f4:4e:fc:05:1b:4a", 4))
+
+try:
+    while True:
+        message = input("Enter message: ")
+        client.send(message.encode("utf-8"))
+        data = client.recv(1024)
+        if not data: 
+            break
+        print(f"Message: {data.decode('utf-8')}")
+except OSError as e:
+    pass
+
+client.close()
